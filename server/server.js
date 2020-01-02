dcec240735b7c76357d22579c8b2be8ac7e051ce
Christopher De Leon <cdeleon@forbes.com>
2020-01-01 20:19:40 -0500

    add second route and nodemon for rebuilding serverside code

--- server/server.js
+++ server/server.js
@@ -5,5 +5,6 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, '../dist/index.html')));
+app.get('/route2', (req, res) => res.sendFile(path.join(__dirname, '../dist/route2.html')));
 
 app.listen(port, () => console.log(`Example App listening on port ${port}`));
